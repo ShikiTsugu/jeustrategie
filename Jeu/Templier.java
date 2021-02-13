bdb8d520c0f9b849546ee3aa8cd1bbcc6a8f528b
QUACH kevin <kevin.quach01@etu.univ-paris-diderot.fr>
2021-02-13 15:25:40 +0100

    Update templier.java

---
+++ Jeu/Templier.java
@@ -0,0 +1,12 @@
+public class Templier extends Unite{
+    
+    public Templier(){//prend en argument Joueur joueur
+        //super(joueur);
+        sante = 100; //discussion en cours
+        attaque = 10; //discussion en cours
+        coutUnite = 200; //discussion en cours
+        porteeDeplacement = 3; //discussion en cours
+        porteeAttaque = 5; //discussion en cours
+        pointAction = 2; //discussion en cours
+    }
+}
--- Jeu/templier.java
+++
@@ -1,3 +0,0 @@
-public class templier extends Unite{
-
-}
