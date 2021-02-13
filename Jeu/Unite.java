65d04749704d26c1153f3e13b0176dd087e0d291
QUACH kevin <kevin.quach01@etu.univ-paris-diderot.fr>
2021-02-13 15:08:10 +0100

    Update Unite.java

--- Jeu/Unite.java
+++ Jeu/Unite.java
@@ -1,5 +1,13 @@
-public class Unite {
-
+public abstract class Unite {
+    private int santé;
+    private int attaque;
+    private int coutUnite;
+    private int portéeDeplacement;
+    private int portéeAttaque;
+    private int pointAction;
+    //rajouter plus tard private Joueur joueur;
+    //rajouter plus tard private Case positionUnite;
+    //rajouter plus tard private AlterationEtat etat;
     public Unite(){
 
     }
