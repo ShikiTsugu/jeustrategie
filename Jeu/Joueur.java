6180bd888b859a27bbab7ccc77d328dda456b32f
HOU maxime <wtfomg@live.fr>
2021-02-13 17:42:25 +0100

    Ajout méthode pour ajouter une unité dans la liste du joueur

--- Jeu/Joueur.java
+++ Jeu/Joueur.java
@@ -15,4 +15,21 @@ public class Joueur {
     public void initialiseListeUnites(Terrain t){
         unites = new Unite[t.getMaxUnits()];
     }
+
+    public boolean ajoutUnite(Unite u){
+        //si la liste d'unite est vide, ajoute l'unite en première position
+        if(unites[0]==null){
+            unites[0]=u;
+            return true;
+        }
+        //sinon parcourt le tableau et cherche une position vide pour y ajouter l'unite
+        for (Unite un : unites) {
+            if (un == null) {
+                un = u;
+                return true;
+            }
+        }
+        //s'il n'y a plus de place, retourne faux
+        return false;
+    }
 }
