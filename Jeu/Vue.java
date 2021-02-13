39abe2614b8f0c73ecbf449228d0953e1893672b
sebastien <sebastienzhang864@gmail.com>
2021-02-13 16:18:19 +0100

    RE:fonction basique vue

--- Jeu/Vue.java
+++ Jeu/Vue.java
@@ -1,9 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.util.ArrayList;
 
-public class Vue {
+public class Vue extends JFrame{
 
     private ImagePane imagePane;
-    private IntGraphModel model;
-    private JLabel Titre = new JLabel();
+    private Model model;
+    private JPanel panel = new JPanel();
+    
+    public Vue(Model m){
+    	model = m;
+    	imagePane = new ImagePane();
+        setTitle("Test");
+        setSize(1680,1050);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+    
+    public class ImagePane extends JPanel{
+        public ImagePane(){
+            setPreferredSize(new Dimension(model.getImage().getWidth(), model.getImage().getHeight()));
+        }
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(model.getImage(),0,0,this);
+        }
+    }
 
 }
