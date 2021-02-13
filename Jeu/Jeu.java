9295db3911f1a9a0ccadb3e9fc9a94cf786b3e38
CINTERO francois <francois.cintero@live.fr>
2021-02-13 18:02:36 +0100

    ajout de méthode de prise en charge de requêtes joueur

--- Jeu/Jeu.java
+++ Jeu/Jeu.java
@@ -54,6 +54,12 @@ public class Jeu {
         this.terrain = terrain;
     }
 
+
+    /**
+     * fonction qui verifie si la partie est finie
+     * @return le boolean disant si la partie est finie
+     * A MODIFIER
+     */
     public boolean gameIsOver(){
         if(joueur1.getHero().getPv() <= 0 || joueur2.getHero().getPv() <=0) 
             return true;
@@ -61,15 +67,83 @@ public class Jeu {
         return false;
     }
 
+    /**
+     * fonction d'initialisation de debut de partie
+     * @param joueur1 le joueur 1
+     * @param joueur2 le joueur 2
+     * @param terrain le terrain choisi
+     * A MODIFIER
+     */
     public void startNewGame(Joueur joueur1 ,Joueur joueur2, Terrain terrain){
         setJoueur1(joueur1);
         setJoueur2(joueur2);
         setTerrain(terrain);
     }
 
-    public void joueur1Play(){
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Fonction de requètes joueur
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+    /**
+     * fonction qui prend en charge les requête d'achats d'unite
+     * @param joueur le joueur qui a effectué la requête
+     * @param unite l'unite à place sur le terrain
+     * @param x la coordonné x où place l'unite
+     * @param y la coordonné y où place l'unite
+     * @return le boolean indiquant la réussite de la requête
+     * A MODIFIER
+     */
+    public boolean acheteUnite(Joueur joueur ,Unite unite, int x , int y){
+        //verifie si le joueur possède assez d'argent
+        if(joueur.getArgent()< unite.getCout())
+            return false;
+        //fait appele à la fonction d'ajout d'unite sur le terrain
+        if(!terrain.ajouteUnite( unite, x,y)){
+            return false;
+        }
+        //fait appele à la fonction d'ajout dans l'armée du joueur
+        return joueur.ajouteUnite(unite);
     }
+
+
+    /**
+     * fonction qui prend en charge les requête de deplacement d'unite
+     * @param joueur le joueur qui a effectué la requête
+     * @param unite l'unite à place sur le terrain
+     * @param x la coordonné x où place l'unite
+     * @param y la coordonné y où place l'unite
+     * @return le boolean indiquant la réussite de la requête
+     */
+    public boolean deplaceUnite(Joueur joueur ,Unite unite, int x, int y){
+        //verifie si l'unite appartient au joueur
+        if(joueur == unite.getJoueur())
+            return terrain.deplaceUnite(unite , x ,y);
+        return false;
+    }
+
+    /**
+     * fonction qui prend en charge les requête d'attaque d'unite
+     * @param joueur le joueur qui a effectué la requête
+     * @param unite l'unite à place sur le terrain
+     * @param x la coordonné x où place l'unite
+     * @param y la coordonné y où place l'unite
+     * @return le boolean indiquant la réussite de la requête
+     */
+    public boolean attaqueUnite(Joueur joueur,Unite unite,int x, int y){
+        //verifie si l'unite appartient au joueur
+        if(joueur != unite.getJoueur())
+            return false;
+        //verifie si l'emplacement à une unité puis si cette dernière est du camp opposé
+        if(terrain.getUnite(x,y) == null || terrain.getUnite(x,y).getJoueur() == joueur )
+            return false;
+        
+        return terrain.attaqueUnite(unite, terrain.getUnite(x,y));
+    }
+
+
+
+
 
 
 
