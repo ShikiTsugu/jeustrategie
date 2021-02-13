cd70d37bb56198466c9ab7db02f1cde34757af4a
HOU maxime <wtfomg@live.fr>
2021-02-13 17:17:30 +0100

    Ajout valeur max possible d'unites sur le terrain

--- Jeu/Terrain.java
+++ Jeu/Terrain.java
@@ -1,13 +1,16 @@
 public class Terrain {
     protected Case[][] plateau;
+    protected int maxUnits;
 
-    public Terrain(int largeur, int hauteur){
+    public Terrain(int largeur, int hauteur, int max){
         plateau = new Case[hauteur][largeur];
+        maxUnits = max;
     }
 
-    public Terrain(Case[][] p){
+    public Terrain(Case[][] p, int max){
         plateau = p;
+        maxUnits = max;
     }
 
-
+    public int getMaxUnits(){return maxUnits;}
 }
