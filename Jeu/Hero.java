c0d6edebf41b8193a2695ccdf9bb9358eeed14b7
QUACH kevin <kevin.quach01@etu.univ-paris-diderot.fr>
2021-02-17 19:24:52 +0100

    Update Hero.java

--- Jeu/Hero.java
+++ Jeu/Hero.java
@@ -3,6 +3,7 @@ public class Hero extends Unite{
     public Hero(Joueur joueur){
         super(joueur);
         santeMax = 300; //discussion en cours
+        santeCourante = 300; //discusssion en cours
         attaque = 50; //discussion en cours
         coutUnite = 0; //discussion en cours
         porteeDeplacement = 5; //discussion en cours
