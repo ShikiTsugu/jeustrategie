6783f83777d9edfa7185939d250ac18960ed233e
HOU maxime <wtfomg@live.fr>
2021-02-20 15:37:21 +0100

    Ajout d'une méthode pour ajouter une unité sur le terrain

--- Jeu/Terrain.java
+++ Jeu/Terrain.java
@@ -12,5 +12,17 @@ public class Terrain {
         maxUnits = max;
     }
 
+    public boolean ajouteUnite(Unite u, int x, int y){
+        try{
+            if(plateau[y][x]==null){
+                plateau[y][x]=new Case(u);
+                return true;
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            return false;
+        }
+        return false;
+    }
+
     public int getMaxUnits(){return maxUnits;}
 }
