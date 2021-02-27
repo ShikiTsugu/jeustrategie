a1e18cd8de24b496becc63475667ad03eabd2647
QUACH kevin <kevin.quach01@etu.univ-paris-diderot.fr>
2021-02-27 15:52:57 +0100

    Update Archer.java

--- src/com/unite/Archer.java
+++ src/com/unite/Archer.java
@@ -1,2 +1,25 @@
-package com.unite;public class Archer {
+package com.unite;
+
+import com.player.Joueur;
+
+public class Archer extends Unite{
+    
+    public Archer(Joueur joueur){
+        super(joueur);
+        santeMax = 150; //discussion en cours
+        santeCourante = 150;
+        attaque = 30; //discussion en cours
+        coutUnite = 300; //discussion en cours
+        porteeDeplacement = 3; //discussion en cours
+        porteeAttaque = 5; //discussion en cours
+        pointAction = 2; //discussion en cours
+    }
+    
+    public String toString(){
+        return "Archer";
+    }
+    
+    public final boolean isHero(){
+        return false;
+    }
 }
