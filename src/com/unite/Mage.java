e5d7c179f892285469d5e6aa3570a62b8125f3a1
QUACH kevin <kevin.quach01@etu.univ-paris-diderot.fr>
2021-02-27 15:55:53 +0100

    Update Mage.java

--- src/com/unite/Mage.java
+++ src/com/unite/Mage.java
@@ -1,2 +1,25 @@
-package com.unite;public class Mage {
+package com.unite;
+
+import com.player.Joueur;
+
+public class Mage extends Unite{
+    
+    public Mage(Joueur joueur){
+        super(joueur);
+        santeMax = 125; //discussion en cours
+        santeCourante = 125;
+        attaque = 50; //discussion en cours
+        coutUnite = 400; //discussion en cours
+        porteeDeplacement = 4; //discussion en cours
+        porteeAttaque = 5; //discussion en cours
+        pointAction = 1; //discussion en cours
+    }
+    
+    public String toString(){
+        return "mage";
+    }
+    
+    public final boolean isHero(){
+        return false;
+    }
 }
