b17422453334131003dcf00b8d14453f9d28f11c
HOU maxime <wtfomg@live.fr>
2021-02-27 15:22:19 +0100

    Nouvelle classe pour les actions du joueur

---
+++ src/com/player/ActionJoueur.java
@@ -0,0 +1,46 @@
+package com.player;
+
+import com.plateau.Case;
+import com.plateau.Terrain;
+import com.unite.Unite;
+
+public class ActionJoueur {
+    private Joueur joueur;
+
+    public ActionJoueur(Joueur j){
+        joueur = j;
+    }
+
+    //Action du joueur pour acheter une unité.
+    public boolean acheteUnite(Unite unite){
+        //si le joueur n'a pas assez d'argent pour acheter l'unité, on retourne false.
+        if(joueur.getArgent()< unite.getCoutUnite()) return false;
+        //sinon on soustrait l'argent du joueur par le cout de l'unité, et on ajoute cette unité dans sa liste d'unité.
+        else{
+            joueur.setArgent(joueur.achat(unite.getCoutUnite()));
+            return joueur.ajouteUnite(unite);
+        }
+    }
+
+    public boolean deplaceUnite(Unite unite, Case destination){
+        //verifie si l'unite appartient au joueur
+        if(joueur == unite.getJoueur()){
+            unite.deplaceUnite(unite , destination);
+            return true ;
+        }
+        return false;
+    }
+
+    public boolean attaqueUnite(Case depart,Case cible){
+        if(depart.getUnite() == null) return false;
+
+        //verifie si l'unite appartient au joueur
+        if(joueur != depart.getUnite().getJoueur()) return false;
+
+        //verifie si l'emplacement à une unité puis si cette dernière est du camp opposé
+        if(cible.getUnite() == null || cible.getUnite().getJoueur() == joueur) return false;
+
+        depart.getUnite().attaqueUnite(depart, cible);
+        return true;
+    }
+}
