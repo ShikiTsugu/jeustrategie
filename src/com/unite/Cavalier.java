7b1186b9e166013714a073ba8f6f818cea4bd974
QUACH kevin <kevin.quach01@etu.univ-paris-diderot.fr>
2021-02-27 15:51:40 +0100

    Update Cavalier.java

--- src/com/unite/Cavalier.java
+++ src/com/unite/Cavalier.java
@@ -1,2 +1,25 @@
-package com.unite;public class Cavalier {
+package com.unite;
+
+import com.player.Joueur;
+
+public class Cavalier extends Unite{
+    
+    public Templier(Joueur joueur){
+        super(joueur);
+        santeMax = 200; //discussion en cours
+        santeCourante = 200;
+        attaque = 50; //discussion en cours
+        coutUnite = 500; //discussion en cours
+        porteeDeplacement = 6; //discussion en cours
+        porteeAttaque = 1; //discussion en cours
+        pointAction = 3; //discussion en cours
+    }
+    
+    public String toString(){
+        return "templier";
+    }
+    
+    public final boolean isHero(){
+        return false;
+    }
 }
