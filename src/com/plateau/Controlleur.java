187aacee17ab8bd4478b1dfc17805bf81104d234
HOU maxime <wtfomg@live.fr>
2021-03-10 15:08:39 +0100

    ajout méthode pour placer une unité sur la case cliqué

--- src/com/plateau/Controlleur.java
+++ src/com/plateau/Controlleur.java
@@ -1,6 +1,10 @@
 package com.plateau;
 
 import com.player.*;
+import com.unite.Unite;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class Controlleur {
     private Vue vue;
@@ -8,4 +12,20 @@ public class Controlleur {
     public Controlleur(Vue v){
         vue = v;
     }
+
+    public boolean uniteAchete(Unite u, Joueur j){
+        for(Unite un : j.getUnites()){
+            if(un==u) return true;
+        }
+        return false;
+    }
+
+    public void placeUniteApresAchat(Unite u, ActionJoueur j){
+        if(uniteAchete(u,j.getJoueur())){
+            for(JButton b : vue.terrainBt){
+                b.addActionListener((ActionEvent e) -> j.placeUnite(vue.terrain,u,b.getX(),b.getX()));
+            }
+        }
+    }
+
 }
