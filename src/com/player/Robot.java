ddca37f48dc2fecf5c3d3969501273ab68912734
HOU maxime <wtfomg@live.fr>
2021-04-07 12:23:32 +0200

    Ajout méthode choix unite par le robot

--- src/com/player/Robot.java
+++ src/com/player/Robot.java
@@ -6,36 +6,42 @@ import com.unite.Unite;
 import java.util.Random;
 
 public class Robot extends Joueur{
+    private int[] coordTarget = new int[2];
+    private int[] coord = new int[2];
     public Robot(int a){
         super(a);
         isHuman = false;
     }
 
-    public Unite randomUnit(){
-        int range = 0;
-        for (int i = 0; i<this.getUnites().length;i++){
-            if(this.getUnites()[i]==null){
-                break;
-            }
-            range++;
-        }
-        Random rand = new Random();
-        int random = rand.nextInt(range - 1);
-        return this.getUnites()[random];
+    public int[] getCoordTarget(){return coordTarget;}
+
+    public int[] getCoord(){return coord;}
+
+    public void setCoordTarget(int x, int y){
+        coordTarget[0]=x;
+        coordTarget[1]=y;
     }
 
-    public int[] unitCoord(Terrain t){
-        Unite u = randomUnit();
-        int[] coord = new int[2];
+    public boolean pickUnit(Terrain t){
         for(int x = 0; x<t.getPlateau().length; x++){
             for(int y = 0; y<t.getPlateau()[x].length; y++){
-                if(t.getPlateau()[x][y].getUnite()==u){
-                    coord[0]=x;
-                    coord[1]=y;
-                    break;
+                if(t.getPlateau()[x][y].estUnit()){
+                    System.out.println("test1");
+                    System.out.println(t.getPlateau()[x][y].getUnite().getJoueur()==this);
+                    if(t.getPlateau()[x][y].getUnite().getJoueur()==this) {
+                        if(t.getPlateau()[x][y].getUnite().getPointAction()>0) {
+                            System.out.println("test2");
+                            coord[0] = x;
+                            coord[1] = y;
+                            System.out.println("L'unite choisit : " + t.getPlateau()[coord[0]][coord[1]].getUnite());
+                            return true;
+                        }else{
+                            System.out.println("Cette unite n'a plus de PA.");
+                        }
+                    }
                 }
             }
         }
-        return coord;
+        return false;
     }
 }
