136687263a78617fdd87d122e445acff0bb0ff7d
hou <wtfomg@live.fr>
2021-04-30 19:00:22 +0200

    Ajout méthodes pour gérer l'attaque du robot, détection etc..

--- src/com/player/Robot.java
+++ src/com/player/Robot.java
@@ -9,21 +9,16 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class Robot extends Joueur{
-    private int[] coordTarget = new int[2];
+    private LinkedList<int[]> coordTarget = new LinkedList<>();
     private int[] coord = new int[2];
     public Robot(int a, Jeu jeu){
         super(a,jeu);
         isHuman = false;
     }
 
-    public int[] getCoordTarget(){return coordTarget;}
+    public LinkedList<int[]> getCoordTarget(){return coordTarget;}
 
     public int[] getCoord(){return coord;}
-
-    public void setCoordTarget(int x, int y){
-        coordTarget[0]=x;
-        coordTarget[1]=y;
-    }
 
     public boolean pickUnit(Terrain t){
         for(int x = 0; x<t.getPlateau().length; x++){
@@ -67,13 +62,90 @@ public class Robot extends Joueur{
         return coord;
     }
 
+    public LinkedList availableSpaceAroundTarget(Terrain t, Unite u){
+        LinkedList<int[]> coord = new LinkedList<>();
+        //coordonnées linéaires :
+        if(u.estDansTableau(t,u.getCurrentX(),u.getCurrentY()+1)
+                && t.getPlateau()[u.getCurrentY()+1][u.getCurrentX()].estVide()){
+            int[] tmp = {u.getCurrentX(),u.getCurrentY()+1};
+            coord.add(tmp);
+        }
+        if(u.estDansTableau(t,u.getCurrentX(),u.getCurrentY()-1)
+                && t.getPlateau()[u.getCurrentY()-1][u.getCurrentX()].estVide()){
+            int[] tmp = {u.getCurrentX(),u.getCurrentY()-1};
+            coord.add(tmp);
+        }
+        if(u.estDansTableau(t,u.getCurrentX()+1,u.getCurrentY())
+                && t.getPlateau()[u.getCurrentY()][u.getCurrentX()+1].estVide()){
+            int[] tmp = {u.getCurrentX()+1,u.getCurrentY()};
+            coord.add(tmp);
+        }
+        if(u.estDansTableau(t,u.getCurrentX()-1,u.getCurrentY())
+                && t.getPlateau()[u.getCurrentY()][u.getCurrentX()-1].estVide()){
+            int[] tmp = {u.getCurrentX()-1,u.getCurrentY()};
+            coord.add(tmp);
+        }
+        //coordonnées diagonales :
+        if(u.estDansTableau(t,u.getCurrentX()+1,u.getCurrentY()+1)
+                && t.getPlateau()[u.getCurrentY()+1][u.getCurrentX()+1].estVide()){
+            int[] tmp = {u.getCurrentX()+1,u.getCurrentY()+1};
+            coord.add(tmp);
+        }
+        if(u.estDansTableau(t,u.getCurrentX()+1,u.getCurrentY()-1)
+                && t.getPlateau()[u.getCurrentY()-1][u.getCurrentX()+1].estVide()){
+            int[] tmp = {u.getCurrentX()+1,u.getCurrentY()-1};
+            coord.add(tmp);
+        }
+        if(u.estDansTableau(t,u.getCurrentX()-1,u.getCurrentY()-1)
+                && t.getPlateau()[u.getCurrentY()-1][u.getCurrentX()-1].estVide()){
+            int[] tmp = {u.getCurrentX()-1,u.getCurrentY()-1};
+            coord.add(tmp);
+        }
+        if(u.estDansTableau(t,u.getCurrentX()-1,u.getCurrentY()+1)
+                && t.getPlateau()[u.getCurrentY()+1][u.getCurrentX()-1].estVide()){
+            int[] tmp = {u.getCurrentX()-1,u.getCurrentY()+1};
+            coord.add(tmp);
+        }
+        return coord;
+    }
+
+    public boolean canAttack(Terrain t, int x, int y, int portee, Unite u, Joueur j){
+        if(u.estDansTableau(t, x, y)
+                && t.getPlateau()[y][x].estUnit()
+                && t.getPlateau()[y][x].getUnite().getJoueur() != j
+                && portee>=0) {
+            System.out.println("Cible à portée");
+            return true;
+        }
+        if (portee >= 0 && u.estDansTableau(t, x, y)){
+            if (canAttack(t, x -1, y, portee -1, u, j)
+                    && u.estDansTableau(t, x -1, y)){
+                return true;
+            }
+            if (canAttack(t, x + 1, y, portee -1, u, j)
+                    && u.estDansTableau(t, x +1, y)){
+                return true;
+            }
+            if (canAttack(t, x, y-1, portee -1, u, j)
+                    && u.estDansTableau(t, x, y-1)){
+                return true;
+            }
+            if (canAttack(t, x, y +1, portee -1, u, j)
+                    && u.estDansTableau(t, x, y+1)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public boolean targetDetected (Terrain t, int x, int y, int portee, Unite u, Joueur j){
         if(u.estDansTableau(t, x, y)
                 && t.getPlateau()[y][x].estUnit()
                 && t.getPlateau()[y][x].getUnite().getJoueur() != j
                 && portee>=0) {
-            setCoordTarget(x, y);
-            System.out.println("caught");
+            int[] tmp = {x,y};
+            coordTarget.add(tmp);
+            System.out.println("caught "+t.getPlateau()[y][x].getUnite());
             return true;
         }
         if (portee >= 0 && u.estDansTableau(t, x, y)){
