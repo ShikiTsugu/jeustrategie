37c4fd0fb132c53b559acb7735e25b047b644ce5
CINTERO François <francois.cintero@live.fr>
2021-05-05 15:13:12 +0200

    ajout des restriction d'utilisation du BuffProphete

--- src/com/unite/Prophete.java
+++ src/com/unite/Prophete.java
@@ -1,8 +1,13 @@
 package com.unite;
 
+import com.plateau.Terrain;
 import com.player.Joueur;
 
+import java.util.HashMap;
+
 public class Prophete extends Unite{
+
+    protected Unite unite_buffed;
     public Prophete(Joueur joueur){
         super(joueur);
         santeMax = 500; //discussion en cours
@@ -18,12 +23,27 @@ public class Prophete extends Unite{
         competences[0] = new Competence("tir maquique","lancer un projectile magique",event, 5,1, 0);
         Evenement[] event2 = {new Evenement("donnePa",0,0,150,joueur)};
         competences[1] = new Competence("donationPA","redonne de la force à une unitée, lui confère 1 PA supplémentaire",event2, 4,1, 3);
-        Evenement[] event3 = {new Evenement("appliqueBuffProphete", 0, 0, 1,joueur)};
+        Evenement[] event3 = {new Evenement("appliqueBuffProphete", 0, 0, 999,joueur)};
         competences[2] = new Competence("La voie du Seigneur", "applique brièvement la puissance du Seigneur, attaque, portée de déplacement, portée d'attaque et santé courante multipliées par 2!", event3, 4, 2, 2);
     }
 
     public String toString(){
         return "Prophete";
+    }
+
+    public void utiliseCompetence(int xA, int yA, int xD,int yD,int c, Terrain t){
+        HashMap<String,Integer> recap = new HashMap<String,Integer>();
+        if (c >=0 && c < competences.length) {
+            if(unite_buffed != null && c ==2 && unite_buffed.estDecede() == false){
+                return;
+            }
+
+            recap = competences[c].useSkill(xA, yA, xD, yD, t);
+            if(recap.containsKey("success")){
+                unite_buffed = t.getPlateau()[yD][xD].getUnite();
+            }
+
+        }
     }
 
     public final boolean isHero(){
